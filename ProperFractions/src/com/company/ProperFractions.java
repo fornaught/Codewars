2d7aa15fda02ea6d7db542d493ede68301ad1aa4
fornaught <edmelissen@hotmail.com>
2018-08-22 00:00:57 +0200

    nah

--- ProperFractions/src/com/company/ProperFractions.java
+++ ProperFractions/src/com/company/ProperFractions.java
@@ -1,22 +1,39 @@
 package com.company;
 
+
 import java.util.LinkedList;
 import java.util.List;
 
 public class ProperFractions {
     public static long properFractions(long n) {
-        System.out.println("input " + n);
-        long counter = 0;
-        List<Long> count2 = new LinkedList<>();
-        for (long i = 1; i <= 10 || i < n/2; i++) {
-            if ((i % n == 0)) {
-                counter++;
-                if (!count2.contains(i));
-                {
-                    count2.add(i);
-                }
-            }
+        for (int i = 3; i < n; i+=2) {
+
         }
-        return count2.size();
+        return 0;
     }
 }
+
+
+
+
+//    List<Long> fractions = new LinkedList<>();
+//        System.out.println("input " + n);
+//                long temp = 1;
+//                long result = n;
+//                for (long i = n /2; i > 2; i /= 2) {
+//                System.out.println(i);
+//                if (i % 1 == 0) {
+//                for (int j = 1; temp < n/2; j ++){
+//        temp = j;
+//        for (;temp < n/2; temp++)
+//        if (!fractions.contains(temp))
+//        {
+//        fractions.add(temp);
+//        }
+//        temp+= temp;
+//        System.out.println(temp);
+//        }
+//        }
+//        }
+//        System.out.println(fractions.size());
+//        return result - fractions.size();
