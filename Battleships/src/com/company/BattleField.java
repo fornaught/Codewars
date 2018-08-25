e2711361256d3650a1e5544e94a5ec5879856df7
Ed <changethefrequency@hotmail.com>
2018-08-25 19:43:58 +0200

    bship update 1

--- Battleships/src/com/company/BattleField.java
+++ Battleships/src/com/company/BattleField.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 public class BattleField {
 
+    static List<String> ships = new ArrayList<>();
+
     public static boolean fieldValidator(int[][] field) {
 
         return false;
@@ -12,12 +14,11 @@ public class BattleField {
 
     public static int[][] detectShipsHorizontal(int[][] field) {
         for (int[] array : field) {
-            List<String> ships = new ArrayList<>();
+            int shipCounter = 0;
             for (int i : array) {
-                int shipCounter = 0;
                 if (array[i] == 1) {
                     shipCounter++;
-                } else if (array[i] == 0 && shipCounter > 0) {
+                } else if ((array[i] == 0 || i == array.length -1) && shipCounter > 0) {
                     switch (shipCounter) {
                         case 1:
                             array[i - 1] = 5;
@@ -51,29 +52,29 @@ public class BattleField {
         for (int i = 0; i < 10 ; i++){
             for (int j = 0; j < 10; j++) {
                 int shipCounter = 0;
-                if (field[j][i] == 1) {
+                if (field[j][i] == 1 || field[j][i] == 5) {
                     shipCounter++;
-                } else if (field[j][i] == 0 && shipCounter > 0) {
+                } else if ((field[j][i] == 0 || j == 9) && shipCounter > 0) {
                     switch (shipCounter) {
                         case 1:
-                            array[i - 1] = 5;
+                            field[j - 1][i] = 5;
                             break;
                         case 2:
-                            array[i - 1] = 2;
-                            array[i - 2] = 2;
+                            field[j - 1][i] = 2;
+                            field[j - 2][i] = 2;
                             ships.add("Sweeper");
                             break;
                         case 3:
-                            array[i - 1] = 3;
-                            array[i - 2] = 3;
-                            array[i - 3] = 3;
+                            field[j - 1][i] = 3;
+                            field[j - 2][i] = 3;
+                            field[j - 3][i] = 3;
                             ships.add("Destroyer");
                             break;
                         case 4:
-                            array[i - 1] = 4;
-                            array[i - 2] = 4;
-                            array[i - 3] = 4;
-                            array[i - 4] = 4;
+                            field[j - 1][i] = 4;
+                            field[j - 2][i] = 4;
+                            field[j - 3][i] = 4;
+                            field[j - 4][i] = 4;
                             ships.add("Cruiser");
                             break;
                     }
@@ -85,6 +86,11 @@ public class BattleField {
     }
 
     public static boolean clearWaters(int[][] field){
-
+        for (int i = 1; i < 10; i++){
+            for (int j = 0; j < 10; j++){
+                //count 2*2, give ships unigue sum values i.e. cruiser = 1000, destroyer = 500, etc.
+            }
+        }
+        return false;
     }
 }
